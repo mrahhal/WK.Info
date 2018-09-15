18d27637df776445c8b9114e5035abf4685d1fbf
Mohammad Rahhal <m.r992@hotmail.com>
2018-09-15 14:16:29 +0300

    Initial commit

---
+++ WK.Info/Models.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace WK.Info
+{
+	public class UserInformation
+	{
+		public int Level { get; set; }
+		public string Title { get; set; }
+		public string Username { get; set; }
+	}
+
+	public class Vocabulary
+	{
+		public int Level { get; set; }
+		public string Character { get; set; }
+		public string Kana { get; set; }
+		public string Meaning { get; set; }
+	}
+
+	public class VocabularyModelRequestedInformation
+	{
+		public List<Vocabulary> General { get; set; }
+	}
+
+	public class VocabularyModel
+	{
+		public UserInformation UserInformation { get; set; }
+		public VocabularyModelRequestedInformation RequestedInformation { get; set; }
+	}
+}
