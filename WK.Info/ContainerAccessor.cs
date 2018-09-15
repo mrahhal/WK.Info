901f1f91ff3357e3f49271a136db3ebdfa4739e5
Mohammad Rahhal <m.r992@hotmail.com>
2018-09-15 17:39:15 +0300

    Add ContainerAccessor

---
+++ WK.Info/ContainerAccessor.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using WK.Info.Services;
+using IContainer = Autofac.IContainer;
+
+namespace WK.Info
+{
+	public static class ContainerAccessor
+	{
+		private static readonly IContainer _container;
+
+		static ContainerAccessor()
+		{
+			_container = ConfigureServices();
+		}
+
+		public static IContainer Container => _container;
+
+		private static IContainer ConfigureServices()
+		{
+			var builder = new ContainerBuilder();
+
+			builder.RegisterType<DictionaryProvider>().As<IDictionaryProvider>().SingleInstance();
+
+			return builder.Build();
+		}
+	}
+}
